2ae6e6115677dc240bd838049136691feac562b1
Jieun Lee <2ur.leeje@gmail.com>
2025-02-27 14:05:16 +0900

    dev branch

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -8,21 +8,23 @@ import MyReview from '../pages/MyReview';
 import EditProfile from '../pages/EditProfile';
 import TripFinder from '../pages/TripFinder';
 import ReviewEditor from '../pages/ReviewEditor';
+import TestPage from '../pages/TestPage';
 
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/my-page' element={<MyPage />}>
-            <Route path='my-review' element={<MyReview />} />
-            <Route path='edit-profile' element={<EditProfile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/my-page" element={<MyPage />}>
+            <Route path="my-review" element={<MyReview />} />
+            <Route path="edit-profile" element={<EditProfile />} />
           </Route>
-          <Route path='/trip-finder' element={<TripFinder />} />
-          <Route path='/review-editor' element={<ReviewEditor />} />
+          <Route path="/trip-finder" element={<TripFinder />} />
+          <Route path="/review-editor" element={<ReviewEditor />} />
+          <Route path="/test" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
     </div>
