16a99660c88653ad44a175f911c4f4a52ab4bfc6
usuny0317 <usuny0317@naver.com>
2025-02-27 19:31:46 +0900

    ✨feat 검색 값을 아직 받아오지 못해 하드 코딩으로  ''서울관광로 작성했습니다 #2

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -1,45 +1,52 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 const key = 'AIzaSyD06auha20lnXQsMPJjUsAlpH3uGo38UNY';
 
 const Youtube = () => {
-  const handleVideo = async () => {
-    try {
-      //axios를 통한 api 연결
-      //검색은 get ()
-      //v3 api사용 https://www.googleapis.com/youtube/v3 요게 주소! 전에
-      // 제공해주신 nbcamp? 같은 서버 느낌
-      // /search 이 친구가 login 같은 api에서 제공하는 검색 느낌!
-      //저희는 검색 밖에 없으니까 일단 나누지 않겠습니다
+  const [videos, setVideos] = useState([]);
 
-      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          part: 'snippet', //snippet을 설정하는 경우 API 응답은 하위 속성도 모두 포함
-          maxResults: 3, // 상위 3개 영상 가져오기
-          q: '서울', // 검색어
-          type: 'video', // 영상만 필터링
-          key: key,
-        },
-      });
-      return response.data.items;
-    } catch (err) {
-      alert('영상을 불러옴에 오류가 있습니다!' + err);
-      return [];
-    }
-  };
+  useEffect(() => {
+    const handleVideo = async () => {
+      try {
+        //axios를 통한 api 연결
+        //검색은 get ()
+        //v3 api사용 https://www.googleapis.com/youtube/v3 요게 주소! 전에
+        // 제공해주신 nbcamp? 같은 서버 느낌
+        // /search 이 친구가 login 같은 api에서 제공하는 검색 느낌!
+        //저희는 검색 밖에 없으니까 일단 나누지 않겠습니다
+
+        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            part: 'snippet', //snippet을 설정하는 경우 API 응답은 하위 속성도 모두 포함
+            maxResults: 3, // 상위 3개 영상 가져오기
+            q: '서울 관광지', // 검색어
+            type: 'video', // 영상만 필터링
+            key: key,
+          },
+        });
+        setVideos(response.data.items);
+      } catch (err) {
+        alert('영상을 불러옴에 오류가 있습니다!' + err);
+      }
+    };
+    handleVideo();
+  }, []);
 
   /* 
   이렇게 바로 받아오면 오류나더라...
   await 인데 handleVideo()가 Promise(비동기 함수)인데, videos.map()을 바로 호출해서 handleVideo()는 비동기 함수(async function)라서 즉시 데이터를 반환하는 게 아니라 
   Promise 객체를 반환... videos.map()을 하려고 하니까, videos가 배열이 아니라 Promise라서 에러남.. >>useEffect 로 페이지 로딩 될 때마다 값 계산해서 적용하는 게 좋을 듯?
 
+  근데 나 response.data.items; 이렇게 받아온 거면 값 받아온 거 맞지 않냐
   const videos =  handleVideo(); */
   return (
     <div>
       Youtube
       <div>
-        {/* {!videos ? (
-          <div>불러오는데 오류났음! 값이 없음!</div>
+        {videos.length === 0 ? (
+          <div>불러오는데 오류났음!길이가 0!</div>
         ) : (
           //들어오는 형태: [ { id: { videoId: "123" }, snippet: { title: "제목..", thumbnails: { medium: { url: "썸네일 사진 같아용.jpg" } } } },]
           //영상 링크 형태: https://www.youtube.com/embed/ + videoId
@@ -54,28 +61,7 @@ const Youtube = () => {
               ></iframe>
             </div>
           ))
-        ) }*/}
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/an9J6isFQag"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/J1DHOQNhU6c"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/tgKs6glo5zk"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
+        )}
       </div>
     </div>
   );
