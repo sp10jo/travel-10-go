4bc9aed448783e719c228dd61b9ea62eb48c72c1
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-28 14:20:01 +0900

    feat: TestPage 내용 TripFinder에 적용

--- src/pages/TripFinder.jsx
+++ src/pages/TripFinder.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
+import ReviewViewer from '../components/ReviewViewer';
 
 const TripFinder = () => {
-  return <div>TripFinder</div>;
+  const [openReviewViewer, setOpenReviewViewer] = useState(false);
+  const [placeId, setPlaceId] = useState('');
+
+  return (
+    <div className="flex flex-col">
+      {openReviewViewer && <ReviewViewer placeId={placeId} setOpenReviewViewer={setOpenReviewViewer} />}
+    </div>
+  );
 };
 
 export default TripFinder;
