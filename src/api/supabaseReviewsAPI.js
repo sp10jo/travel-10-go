bd6d6035173ddf6e58ee45bc5845644d60eaca4f
taejin7088 <gog70888@gmail.com>
2025-02-28 16:27:59 +0900

    ✨feat: 리뷰데이터로 카드 모양생성

--- src/api/supabaseReviewsAPI.js
+++ src/api/supabaseReviewsAPI.js
@@ -1,11 +1,10 @@
 import supabase from '../supabase/client';
 
+const REVIEWS_SELECT_QUERY = '* , imgs:id(*), places(place_address,place_name), users(*)';
+
 //플레이스의 id를 기준으로 리뷰를 가져오기
 export const getReviewsByPlaceId = async (placeId) => {
-  const { data, error } = await supabase
-    .from('reviews')
-    .select('* , imgs:id(*), places(place_address,place_name)')
-    .eq('place_id', placeId);
+  const { data, error } = await supabase.from('reviews').select(REVIEWS_SELECT_QUERY).eq('place_id', placeId);
   if (error) {
     throw error;
   }
@@ -14,10 +13,7 @@ export const getReviewsByPlaceId = async (placeId) => {
 
 //유저의 userId를 기준으로 리뷰를 가져오기
 export const getReviewsByUserId = async (userId) => {
-  const { data, error } = await supabase
-    .from('reviews')
-    .select('* , imgs:id(*), places(place_address,place_name)')
-    .eq('user_id', userId);
+  const { data, error } = await supabase.from('reviews').select(REVIEWS_SELECT_QUERY).eq('user_id', userId);
   if (error) {
     throw error;
   }
