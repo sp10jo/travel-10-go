7b016810ed9d90543e8dc4d47f46c1cbaeae9e67
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-28 12:49:57 +0900

    feat: KakaoMap 컴포넌트 추가

---
+++ src/components/maps/KakaoMap.jsx
@@ -0,0 +1,7 @@
+import { Map } from 'react-kakao-maps-sdk';
+
+const KakaoMap = () => {
+  return <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '100%', height: '600px' }} level={3} />;
+};
+
+export default KakaoMap;
