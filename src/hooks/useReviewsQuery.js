c3a36d887ad4b1513592576bab97b853e02c91cd
taejin7088 <gog70888@gmail.com>
2025-02-28 14:45:27 +0900

    ♻️refactor : useReviewsQuery.js 수정

--- src/hooks/useReviewsQuery.js
+++ src/hooks/useReviewsQuery.js
@@ -4,7 +4,7 @@ import { getReviewsByPlaceId, getReviewsByUserId } from '../api/supabaseReviewsA
 //place_id를 기준으로 리뷰 데이터들을 가져옵니다.
 export const useReviewsByPlaceIdQuery = (placeId) => {
   return useQuery({
-    queryFn: async () => await getReviewsByPlaceId(placeId),
+    queryFn: () => getReviewsByPlaceId(placeId),
     queryKey: ['REVIEWS', placeId],
   });
 };
@@ -12,7 +12,7 @@ export const useReviewsByPlaceIdQuery = (placeId) => {
 //user_id를('a9d4376d-2e65-4be1-ba62-5f6103500751') 기준으로 리뷰 데이터들을 가져옵니다
 export const useReviewsByUserIdQuery = (userId) => {
   return useQuery({
-    queryFn: async () => await getReviewsByUserId(userId),
+    muaFn: () => getReviewsByUserId(userId),
     queryKey: ['REVIEWS', userId],
   });
 };
