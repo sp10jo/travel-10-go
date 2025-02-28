1d01718a96df24fa00f3b9098fdada4eed40229e
taejin7088 <gog70888@gmail.com>
2025-02-28 21:01:09 +0900

    ✨feat: 리뷰지우는 api요청과 뮤테이트작성, 쿼리키상수선언

--- src/hooks/tanstack/useReviewsQuery.js
+++ src/hooks/tanstack/useReviewsQuery.js
@@ -1,18 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { getReviewsByPlaceId, getReviewsByUserId } from '../../api/supabaseReviewsAPI';
+import { QUERY_KEY } from '../../constants/queryKey';
 
 //place_id를 기준으로 리뷰 데이터들을 가져옵니다.
 export const useReviewsByPlaceIdQuery = (placeId) => {
   return useQuery({
     queryFn: () => getReviewsByPlaceId(placeId),
-    queryKey: ['REVIEWS', placeId],
+    queryKey: [QUERY_KEY.REVIEWS, placeId],
   });
 };
 
-//user_id를('a9d4376d-2e65-4be1-ba62-5f6103500751') 기준으로 리뷰 데이터들을 가져옵니다
+//user_id를 기준으로 리뷰 데이터들을 가져옵니다
 export const useReviewsByUserIdQuery = (userId) => {
   return useQuery({
     muaFn: () => getReviewsByUserId(userId),
-    queryKey: ['REVIEWS', userId],
+    queryKey: [QUERY_KEY.REVIEWS, userId],
   });
 };
