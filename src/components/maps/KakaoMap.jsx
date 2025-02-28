564952fd3e547a9e1d10fc0012e9668da4d19c40
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-28 15:11:33 +0900

    refactor: 위도, 경도, zoom 레벨 변수로 분리

--- src/components/maps/KakaoMap.jsx
+++ src/components/maps/KakaoMap.jsx
@@ -1,13 +1,14 @@
 import { Map, ZoomControl } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
+  const lat = 33.450701;
+  const lng = 126.570667;
+  const zoom = 3;
+
   return (
-    <>
-      <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '100%', height: '100%' }} level={3}>
-        <ZoomControl position={'RIGHT'} />
-      </Map>
-      ;
-    </>
+    <Map center={{ lat, lng }} style={{ width: '100%', height: '100%' }} level={zoom}>
+      <ZoomControl position={'RIGHT'} />
+    </Map>
   );
 };
 
