82e842c45ff27600aba88b801f9deefc43803bc1
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-28 15:06:24 +0900

    feat: 지도 zoom control 추가

--- src/components/maps/KakaoMap.jsx
+++ src/components/maps/KakaoMap.jsx
@@ -1,7 +1,14 @@
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, ZoomControl } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
-  return <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '100%', height: '100%' }} level={3} />;
+  return (
+    <>
+      <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '100%', height: '100%' }} level={3}>
+        <ZoomControl position={'RIGHT'} />
+      </Map>
+      ;
+    </>
+  );
 };
 
 export default KakaoMap;
