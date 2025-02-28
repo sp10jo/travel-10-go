bcde2a630491f4337b306856b8530f2cc7654514
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-28 09:48:29 +0900

    test: user에 대한 mock_data 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,28 +2,45 @@ import Button from '../common/Button';
 import Avatar from '../common/Avatar';
 
 const Header = () => {
+  // ===== Test Data (추후 zustand 상태와 연결) =====
   const isLogin = true;
+  const user = {
+    name: 'test',
+    profileImage:
+      'https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?w=740',
+  };
+  // =============================================
+
+  const AVATAR_SIZE = 50;
 
   return (
     <header className="flex justify-between items-center p-4 bg-white shadow-md">
-      <a href="/" className="text-2xl text-blue-500 font-arvo no-underline hover:no-underline">
+      <a href="/" className="text-2xl text-blue-500 font-arvo font-bold no-underline hover:no-underline">
         10Go
       </a>
 
       <div className="flex items-center space-x-4">
         {!isLogin ? (
           <>
-            <a href="/login" className="px-4 py-2 bg-blue-500 rounded text-white hover:bg-blue-600">
+            <a
+              href="/login"
+              className="px-4 py-2 bg-indigo-500 rounded-lg text-white hover:bg-indigo-600 transition duration-300"
+            >
               Login
             </a>
-            <a href="/signup" className="px-4 py-2 bg-gray-500 rounded text-white hover:bg-gray-600">
+            <a
+              href="/signup"
+              className="px-4 py-2 bg-emerald-500 rounded-lg text-white hover:bg-emerald-600 transition duration-300"
+            >
               Sign Up
             </a>
           </>
         ) : (
           <>
-            <Button className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600">Logout</Button>
-            <Avatar />
+            <Button className="px-4 py-2 text-white bg-rose-500 rounded-lg hover:bg-rose-600 transition duration-300">
+              Logout
+            </Button>
+            <Avatar src={user.profileImage} size={AVATAR_SIZE} />
           </>
         )}
       </div>
