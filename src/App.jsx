2045b81a25022a00642cd172e05ca02f162fbfde
taejin7088 <gog70888@gmail.com>
2025-02-28 14:08:11 +0900

    ✨feat: 리뷰데이터 가져오는 슈파베이스 메서드와 텐스텍쿼리 훅파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Router from './shared/Router';
 
 function App() {
   const queryClient = new QueryClient();
+
   //세션확인(세션 잘못 됐으면 로그아웃)
   useAuthListener();
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
