f722fc3f8ad9cfbf264a4571dcc17d14aefbf60b
taejin7088 <gog70888@gmail.com>
2025-03-02 21:23:37 +0900

    fix : 해더가 사진에 가려지는 버그 수정

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Header from './Header';
 const MainLayout = () => {
   return (
     <div className="flex flex-col h-screen">
-      <header className="fixed w-[100%] h-[80px]">
+      <header className="fixed w-[100%] h-[80px] z-50">
         <Header />
       </header>
 
