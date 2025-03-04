66b08a90d95ec63dd86979383edc5d4e8e1df727
UrePu <darkin31313@gmail.com>
2025-03-04 12:46:36 +0900

    feat: button 컴포넌트 강제

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,34 @@
-const Button = ({ onClick, children, className = '', type = 'button' }) => (
-  <button type={type} onClick={onClick} className={`px-4 py-2 border-t rounded ${className}`}>
-    {children}
-  </button>
-);
+const Button = ({ onClick, children, type = 'button', size = 2, textcolor = 'black', bgcolor = 'white' }) => {
+  const sizeClasses = {
+    1: 'px-2 py-1 text-sm', // 작은 사이즈
+    2: 'px-4 py-2 text-base', // 기본 사이즈
+    3: 'px-6 py-3 text-lg', // 큰 사이즈
+  };
+  const bgClasses = {
+    blue: 'bg-blue-500 hover:bg-blue-700',
+    green: 'bg-green-500 hover:bg-green-700',
+    red: 'bg-red-500 hover:bg-red-700',
+    gray: 'bg-gray-500 hover:bg-gray-700',
+    yellow: 'bg-yellow-500 hover:bg-yellow-700',
+    black: 'bg-black hover:bg-gray-900',
+    white: 'bg-white hover:bg-gray-200',
+  };
+
+  const textClass = {
+    white: 'text-white',
+    black: 'text-black',
+    blue: 'text-blue-500',
+  };
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${sizeClasses[size]} ${textClass[textcolor]} border-t rounded ${bgClasses[bgcolor]} shadow-md`}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
