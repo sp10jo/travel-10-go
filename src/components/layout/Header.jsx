6b9c10b905aacfdc86bef6edb8e5c2bb884179d8
usuny0317 <usuny0317@naver.com>
2025-03-04 12:57:12 +0900

    ✨feat/ 헤더 마무리 했습니다. #2

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,6 @@
 import Button from '../common/Button';
 import Avatar from '../common/Avatar';
 import useAuthStore from '../../zustand/authStore';
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -9,19 +8,8 @@ const Header = () => {
   const isLogin = useAuthStore((state) => state.isLogin);
   const user = useAuthStore((state) => state.user);
 
-=======
-
-const Header = () => {
-  // ===== Test Data (추후 zustand 상태와 연결) =====
-  // const isLogin = false;
-  // const user = {
-  //   name: 'test',
-  //   profileImage:
-  //     'https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?w=740',
-  // };
->>>>>>> dev
   // =============================================
-  const { isLogin, user, setLogout } = useAuthStore();
+
   const AVATAR_SIZE = 50;
 
   return (
@@ -49,25 +37,17 @@ const Header = () => {
         ) : (
           <>
             <Button
-<<<<<<< HEAD
               onClick={() => {
                 useAuthStore.getState().setLogout();
                 localStorage.clear();
               }}
-=======
-              onClick={setLogout}
->>>>>>> dev
               className="px-4 py-2 text-white bg-rose-500 rounded-lg hover:bg-rose-600 transition duration-300"
             >
               Logout
             </Button>
-<<<<<<< HEAD
             <Link to={'/my-page'}>
               <Avatar src={user.profile_img_path} size={AVATAR_SIZE} />
             </Link>
-=======
-            {user && <Avatar src={user.profileImage} size={AVATAR_SIZE} />}
->>>>>>> dev
           </>
         )}
       </div>
