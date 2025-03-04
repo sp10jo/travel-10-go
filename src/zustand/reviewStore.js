9e068b4732999ef662ebd23dee2b0e276378edba
taejin7088 <gog70888@gmail.com>
2025-03-04 16:55:09 +0900

    feat : 리뷰쓰기에 위치표시하기 기능추가

--- src/zustand/reviewStore.js
+++ src/zustand/reviewStore.js
@@ -3,8 +3,10 @@ import { create } from 'zustand';
 const useReviewStore = create((set) => ({
   selectedPlace: null,
   openReviewViewer: false,
+  place: null,
   setSelectedPlace: (place) => set({ place, openReviewViewer: true }),
   setOpenReviewViewer: (open) => set({ openReviewViewer: open }),
+  setCloseReviewViewer: () => set({ openReviewViewer: false, place: null }),
 }));
 
 export default useReviewStore;
