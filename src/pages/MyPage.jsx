6ec0809de737f25c4d104875ff7b23f370f9656c
siheon <rlatlgjs357@gmail.com>
2025-03-04 23:01:40 +0900

    탭 메뉴 순서 변경 & 마이리뷰 렌더링

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import NavLink from '../components/common/NavLink';
 import useAuthStore from '../zustand/authStore';
 import Avatar from '../components/common/Avatar';
 
 const MyPage = () => {
   const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  // 마이페이지 진입 시 기본적으로 '마이리뷰' 페이지로 이동
+  useEffect(() => {
+    navigate('my-review');
+  }, [navigate]);
 
   return (
     <div className="w-full max-w-4xl mx-auto p-6">
@@ -21,11 +28,11 @@ const MyPage = () => {
       
       {/* 탭 메뉴 */}
       <div className="flex space-x-4 border-b">
-        <NavLink to="edit-profile" activeClass="border-b-2 border-red-500 font-bold">
-          프로필 수정 페이지
-        </NavLink>
         <NavLink to="my-review" activeClass="border-b-2 border-red-500 font-bold">
           마이리뷰
+        </NavLink>
+        <NavLink to="edit-profile" activeClass="border-b-2 border-red-500 font-bold">
+          프로필 수정 페이지
         </NavLink>
       </div>
 
